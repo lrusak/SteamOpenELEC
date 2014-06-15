c4a283948f8d380dffea3c01a91affead38502e3
Lukas Rusak <lorusak@gmail.com>
2014-06-14 19:59:24 -0700

    Initial Commit, Added semi working python parsing script. Example output is included.

---
+++ parser.py
@@ -0,0 +1,44 @@
+import sys
+from bs4 import BeautifulSoup
+import json
+
+try:
+    import urllib.request as urllib2
+except:
+    import urllib2
+
+json_data = open('GAMES.json')
+data = sorted(json.load(json_data), key=lambda x: [int(x)])
+
+json_data2 = open('GAMES.json')
+data2 = json.load(json_data2)
+
+cookies = [['cookie', 'birthtime=28801'],
+           ['cookie', 'path=/'],
+           ['cookie', 'domain=store.steampowered.com']]
+
+print('AppID\tWorks\tTitle')
+print('---------------------------------------------------------------')
+
+for i in data:
+  try:
+    if data2['%s' % i]['Hidden']:
+      continue
+  except:
+    TypeError
+    
+    url = 'http://store.steampowered.com/app/' + i
+    req = urllib2.Request(url)
+    for cook in cookies:
+      name, value = cook
+      req.add_header(name, value)
+    res = urllib2.urlopen(req)
+    soup = BeautifulSoup(res.read())
+    title = soup.find('div', attrs={'class': 'apphub_AppName'})
+    try:
+      print(i + '\t' + str(data2['%s' % i]) + '\t' + title.contents[0])
+    except:
+      AttributeError
+      
+      print(i + '\tError')
+json_data.close()
